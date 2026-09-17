a70f4ecade4dd1ee99e8d095e431f89ab48be9df
Camila-700 <camiloscas.30.lascano@gmail.com>
2026-09-17 12:07:38 -0500

    Pedir notas con for

--- Taller_1_Funciones/main.go
+++ Taller_1_Funciones/main.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func averageGrade(suma float64, cantidad int) float64 {
 	return suma / float64(cantidad)
 }
+
 func estudiantes() {
 	var cantidad int
 	var nota, total float64
@@ -14,3 +17,11 @@ func estudiantes() {
 		fmt.Println("Ingrese la nota del estudiante", i)
 		fmt.Scan(&nota)
 
+		for nota < 0 || nota > 100 {
+			fmt.Println("Error: la nota debe estar entre 0 y 100")
+			fmt.Println("Ingrese nuevamente la nota:")
+			fmt.Scan(&nota)
+		}
+
+		total += nota
+	}
